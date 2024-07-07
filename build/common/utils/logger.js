6ca9b51454f10792c50fd3ba35dce8a7cc6e1cfc
Covenant Ogowale <justcovenant@outlook.com>
2024-07-07 16:13:46 +0100

    fix: remove build folder

--- build/common/utils/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const logger = (msg) => {
-    console.log(`[LOG]   ${msg}`);
-};
-exports.logger = logger;
-exports.default = exports.logger;
